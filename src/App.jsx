840b8606ac4dfb02a48da7c3890ecdbe816864c5
victor oyiga <victoroyiga01@gmail.com>
2025-08-22 05:11:09 +0100

    working on the rotute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar/Navbar.jsx'
+import  Home from './Page/Home/HomePage'
 import SignUp from './Page/SignUp/SignUp'
 import LogIn from './Page/LogIn/LogIn'
 import Quran from './Page/Quran/Quran_1.jsx'
@@ -11,16 +11,16 @@ function App() {
 
   return (
     <>
-     <Router>
-      <Routes>
-        <Route path="/" element={<Navbar />} />
-        <Route path="/signUp" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/quran_1" element={<Quran />} />
-        <Route path="/prayer" element={<Prayer />} />
-        <Route path="/calendar" element={<Calendar />} />
-      </Routes>
-     </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/quran_1" element={<Quran />} />
+          <Route path="/prayer" element={<Prayer />} />
+          <Route path="/calendar" element={<Calendar />} />
+        </Routes>
+      </Router>
     </>
   )
 }
